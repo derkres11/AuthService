000a1bb20ddef47bd0ba493d67418797d2a7203b
Denis Sadovskii <sadovskidg@mail.ru>
2026-03-08 23:55:46 +0300

    Logger setup

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -3,11 +3,32 @@ package main
 import (
 	"fmt"
 
-	"sso/internal/config"
+	"log/slog"
 )
 
+const (
+	"envlocal" = "local"
+	"envDev" = "dev"
+	"envProd" = "prod"
+)
 func main() {
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
+
+	func setupLogger(env) {
+		var log *slog.Logger
+
+
+		switch env {
+		case envlocal:
+			log = slog.New(slog.NewTextHandler(os.stdout, &slog.HandlerOption{Level: slog.LevelDebug}))
+		case envDev:
+			log = slog.New(slog.NewTextHandler(os.stdout, &slog.HandlerOption{Level: slog.LevelDebug}))
+		case envProd:
+			log = slog.New(slog.NewJSONHandler(os.stdout, &slog.HandlerOption{Level: slog.LevelInfo}))
+		}
+	}
+
+
 }
