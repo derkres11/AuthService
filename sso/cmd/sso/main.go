13fc8130cce3bf7bd28b0c7bb32fdbb90eaae495
Denis Sadovskii <sadovskidg@mail.ru>
2026-03-19 01:12:30 +0300

    Logger connected

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -1,34 +1,36 @@
 package main
 
 import (
-	"fmt"
-
 	"log/slog"
+	"os"
+
+	"sso/internal/config" // Adjust the import path as needed
 )
 
 const (
-	"envlocal" = "local"
-	"envDev" = "dev"
-	"envProd" = "prod"
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
+
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
 
-	func setupLogger(env) {
-		var log *slog.Logger
+	log.Info("Starting the application...")
+}
 
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
 
-		switch env {
-		case envlocal:
-			log = slog.New(slog.NewTextHandler(os.stdout, &slog.HandlerOption{Level: slog.LevelDebug}))
-		case envDev:
-			log = slog.New(slog.NewTextHandler(os.stdout, &slog.HandlerOption{Level: slog.LevelDebug}))
-		case envProd:
-			log = slog.New(slog.NewJSONHandler(os.stdout, &slog.HandlerOption{Level: slog.LevelInfo}))
-		}
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	}
-
-
+	return log
 }
